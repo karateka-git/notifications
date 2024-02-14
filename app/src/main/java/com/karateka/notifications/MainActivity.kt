10f6129df9c11397f4c2ce597af7e93a7406e52f
compadre <zvvlad2@gmail.com>
2024-02-14 14:29:19 +0600

    VE: add permission check

--- app/src/main/java/com/karateka/notifications/MainActivity.kt
+++ app/src/main/java/com/karateka/notifications/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.karateka.notifications
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -19,9 +23,9 @@ import androidx.compose.material3.Text
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.core.R
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.content.ContextCompat
 import com.karateka.notifications.ui.theme.NotificationsTheme
 
 private const val CHANNEL_ID = "TestChannel"
@@ -31,6 +35,16 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            val launcher = rememberLauncherForActivityResult(
+                ActivityResultContracts.RequestPermission()
+            ) { isGranted: Boolean ->
+                if (isGranted) {
+                    createNotification(this@MainActivity)
+                } else {
+                    getPermissionDeniedToast(this@MainActivity).show()
+                }
+            }
+
             NotificationsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
@@ -39,7 +53,26 @@ class MainActivity : ComponentActivity() {
                         horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
                         Text("Нажмите `кнопка` чтобы увидеть уведомление!")
-                        Button(onClick = { createNotification(this@MainActivity) }) {
+                        Button(onClick = {
+                            when {
+                                ContextCompat.checkSelfPermission(
+                                    this@MainActivity, Manifest.permission.POST_NOTIFICATIONS
+                                ) == PackageManager.PERMISSION_GRANTED -> {
+                                    createNotification(this@MainActivity)
+                                }
+                                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) &&
+                                shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
+                                    getPermissionInfoToast(this@MainActivity).show()
+                                }
+                                else -> {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                                        launcher.launch(Manifest.permission.POST_NOTIFICATIONS)
+                                    } else {
+                                        getPermissionDeniedToast(this@MainActivity).show()
+                                    }
+                                }
+                            }
+                        }) {
                             Text("кнопка")
                         }
                     }
@@ -49,6 +82,7 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@SuppressLint("MissingPermission")
 fun createNotification(context: Context) {
     val builder = NotificationCompat.Builder(context, CHANNEL_ID)
         .setSmallIcon(R.drawable.ic_call_answer_low)
@@ -56,9 +90,6 @@ fun createNotification(context: Context) {
         .setContentText("Content text")
         .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
-    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
-        return
-    }
     with(NotificationManagerCompat.from(context)) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             val name = "Уведомления"
@@ -73,3 +104,9 @@ fun createNotification(context: Context) {
         notify(NOTIFICATION_ID, builder.build())
     }
 }
+
+private fun getPermissionDeniedToast(context: Context) =
+    Toast.makeText(context, "Приложению запрещено отправлять уведомления", Toast.LENGTH_LONG)
+
+private fun getPermissionInfoToast(context: Context) =
+    Toast.makeText(context, "Для отправки уведомлений, приложению необходимо разрешение", Toast.LENGTH_LONG)
