ad0f4f962ac2f958aec45e644619763dbc9086e2
compadre <zvvlad2@gmail.com>
2024-02-14 16:59:38 +0600

    VE: add switch for notification permission

--- app/src/main/java/com/karateka/notifications/MainActivity.kt
+++ app/src/main/java/com/karateka/notifications/MainActivity.kt
@@ -5,9 +5,12 @@ import android.annotation.SuppressLint
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Build
 import android.os.Bundle
+import android.provider.Settings
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -15,17 +18,29 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.core.R
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
 import com.karateka.notifications.ui.theme.NotificationsTheme
 
 private const val CHANNEL_ID = "TestChannel"
@@ -44,14 +59,42 @@ class MainActivity : ComponentActivity() {
                     getPermissionDeniedToast(this@MainActivity).show()
                 }
             }
+            var isNotificationEnabled by remember {
+                mutableStateOf(NotificationManagerCompat.from(this@MainActivity).areNotificationsEnabled())
+            }
+            val lifecycleOwner = LocalLifecycleOwner.current
+            val lifecycleState by lifecycleOwner.lifecycle.currentStateFlow.collectAsState()
 
+            LaunchedEffect(lifecycleState) {
+                // Do something with your state
+                // You may want to use DisposableEffect or other alternatives
+                // instead of LaunchedEffect
+                when (lifecycleState) {
+                    Lifecycle.State.DESTROYED -> {}
+                    Lifecycle.State.INITIALIZED -> {}
+                    Lifecycle.State.CREATED -> {}
+                    Lifecycle.State.STARTED -> {}
+                    Lifecycle.State.RESUMED -> {
+                        isNotificationEnabled = NotificationManagerCompat.from(this@MainActivity).areNotificationsEnabled()
+                    }
+                }
+            }
             NotificationsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
                     Column(
+                        modifier = Modifier.padding(horizontal = 16.dp),
                         verticalArrangement = Arrangement.Center,
                         horizontalAlignment = Alignment.CenterHorizontally,
                     ) {
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                        ) {
+                            Text(modifier = Modifier.weight(1f), text = "Получать уведомления")
+                            Switch(checked = isNotificationEnabled, onCheckedChange = { isChecked ->
+                                launchNotificationSettings()
+                            })
+                        }
                         Text("Нажмите `кнопка` чтобы увидеть уведомление!")
                         Button(onClick = {
                             when {
@@ -60,7 +103,7 @@ class MainActivity : ComponentActivity() {
                                 ) == PackageManager.PERMISSION_GRANTED -> {
                                     createNotification(this@MainActivity)
                                 }
-                                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) &&
+                                (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) &&
                                 shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS) -> {
                                     getPermissionInfoToast(this@MainActivity).show()
                                 }
@@ -105,8 +148,25 @@ fun createNotification(context: Context) {
     }
 }
 
+fun Context.launchNotificationSettings() {
+    val intent =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            Intent().apply {
+                action = Settings.ACTION_APP_NOTIFICATION_SETTINGS
+                putExtra(Settings.EXTRA_APP_PACKAGE, packageName)
+            }
+        } else {
+            val uri: Uri = Uri.fromParts("package", packageName, null)
+            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                data = uri
+            }
+        }
+    startActivity(intent)
+}
+
 private fun getPermissionDeniedToast(context: Context) =
-    Toast.makeText(context, "Приложению запрещено отправлять уведомления", Toast.LENGTH_LONG)
+    Toast.makeText(context, "Приложению запрещено отправлять уведомления", Toast.LENGTH_SHORT)
 
 private fun getPermissionInfoToast(context: Context) =
-    Toast.makeText(context, "Для отправки уведомлений, приложению необходимо разрешение", Toast.LENGTH_LONG)
+    Toast.makeText(context, "Для отправки уведомлений, приложению необходимо разрешение", Toast.LENGTH_SHORT)
