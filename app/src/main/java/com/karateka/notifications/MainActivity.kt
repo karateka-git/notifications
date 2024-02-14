6188ab6b39a1833d0495a06285b152f1f0050526
compadre <zvvlad2@gmail.com>
2024-02-14 13:56:58 +0600

    VE: add show notification for API < 33

--- app/src/main/java/com/karateka/notifications/MainActivity.kt
+++ app/src/main/java/com/karateka/notifications/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.karateka.notifications
 
+import android.Manifest
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.R
+import androidx.core.app.ActivityCompat
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.karateka.notifications.ui.theme.NotificationsTheme
+
+private const val CHANNEL_ID = "TestChannel"
+private const val NOTIFICATION_ID = 1
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,25 +34,42 @@ class MainActivity : ComponentActivity() {
             NotificationsTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    Column(
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                    ) {
+                        Text("Нажмите `кнопка` чтобы увидеть уведомление!")
+                        Button(onClick = { createNotification(this@MainActivity) }) {
+                            Text("кнопка")
+                        }
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+fun createNotification(context: Context) {
+    val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+        .setSmallIcon(R.drawable.ic_call_answer_low)
+        .setContentTitle("Title text")
+        .setContentText("Content text")
+        .setPriority(NotificationCompat.PRIORITY_DEFAULT)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NotificationsTheme {
-        Greeting("Android")
+    if (ActivityCompat.checkSelfPermission(context, Manifest.permission.POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+        return
+    }
+    with(NotificationManagerCompat.from(context)) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "Уведомления"
+            val descriptionText = "Тестирую уведомления"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+            // Register the channel with the system.
+            createNotificationChannel(channel)
+        }
+        notify(NOTIFICATION_ID, builder.build())
     }
 }
